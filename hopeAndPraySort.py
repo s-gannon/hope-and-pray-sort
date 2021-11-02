1e7b94e2d6d4ebb0f2d0e307132e9fd0fbdee63b
Spencer <71360107+s-gannon@users.noreply.github.com>
2021-11-02 11:21:52 -0500

    Added docstring

--- hopeAndPraySort.py
+++ hopeAndPraySort.py
@@ -1,4 +1,11 @@
 import random as rand
 def hopeAndPraySort(arr):
+    '''
+    Returns either a sorted list or an unsorted list
+    Parameters:
+        arr: A list
+    Returns:
+        arr: A list containing the same elements which are either sorted or unsorted
+    '''
     rand.shuffle(arr)
     return arr
